13650fbf189d529195a98eafee09ddb32f9d389b
Shyam Guthikonda <shyamguth@gmail.com>
2014-07-03 18:11:43 +0900

    Command line params for directory/port.

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-var server = require('./lib/server');
+#! /usr/bin/env node
 
-server.start();
+var server = require('./lib/server'),
+    program = require('commander'),
+    packageJSON = require('./package.json');
+
+// Command line parsing.
+program
+   .version(packageJSON.version)
+   .usage('<mpq archive path> {options}')
+   .option('-p, --port <n>', 'port number', parseInt);
+
+program.on('--help', function() {
+    console.log('  Examples:');
+    console.log('');
+    console.log('    $ mpq-server /path/to/archive/file.mpq --port 1234');
+    console.log('');
+});
+
+program.parse(process.argv);
+
+var args = program.args;
+if (args.length < 1) {
+    console.error();
+    console.error("  error: mpq archive path not specified");
+
+    program.help();
+}
+
+var mpqPath = program.args[0];
+var port = null;
+if (program.port) {
+    port = program.port;
+}
+
+server.start(mpqPath, port);
